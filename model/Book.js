2467298a4b8e2be6b43a08e940b64aa0b49d5a6b
tanbiranjum <tanbiranjum@hotmail.com>
2022-11-23 23:27:43 +0600

    Book schema added

--- model/Book.js
+++ model/Book.js
@@ -13,9 +13,34 @@ const BookSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please provide a location"],
   },
+  description: {
+    type: String,
+    required: [true, "Please provide a description"],
+  },
   originalPrice: {
     type: Number,
     required: [true, "Please provide an original price"],
   },
-  
+  useOfYears: {
+    type: String,
+    enum: ["less than 1 year", "1-2", "2-3", "3-4", "4-5", "5+"],
+    required: [true, "Please provide a use of years"],
+  },
+  seller: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: [true, "Please provide a seller"],
+  },
+  buyer: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+  },
+  status: {
+    type: String,
+    enum: ["available", "sold"],
+    default: "available",
+  },
 });
+
+const Book = mongoose.model("Book", BookSchema);
+module.exports = Book;
