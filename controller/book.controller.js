61495da30d4e48a374a1b946886269222a2fddcb
tanbiranjum <tanbiranjum@hotmail.com>
2022-11-23 23:45:41 +0600

    book controller added

---
+++ controller/book.controller.js
@@ -0,0 +1,87 @@
+const Book = require("../model/Book");
+
+exports.getBook = async (req, res, next) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    res.status(200).json({
+      status: "success",
+      data: {
+        book,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+
+exports.getAllBooks = async (req, res, next) => {
+  try {
+    const books = await Book.find();
+    res.status(200).json({
+      status: "success",
+      results: books.length,
+      data: {
+        books,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+
+exports.createBook = async (req, res, next) => {
+  try {
+    const newBook = await Book.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: {
+        book: newBook,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+
+exports.updateBook = async (req, res, next) => {
+  try {
+    const book = await Book.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json({
+      status: "success",
+      data: {
+        book,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+
+exports.deleteBook = async (req, res, next) => {
+  try {
+    await Book.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
