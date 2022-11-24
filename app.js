9c91ea210f2277667d337694f8d64cba9d275abf
tanbiranjum <tanbiranjum@hotmail.com>
2022-11-24 14:38:03 +0600

    morgan logger added

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const app = express();
 const bookRouter = require("./routes/book.routes");
 const userRouter = require("./routes/user.routes");
 const authRouter = require("./routes/auth.routes");
+const morgan = require("morgan");
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.use("/api/v1/books", bookRouter);
 app.use("/api/v1/users", userRouter);
